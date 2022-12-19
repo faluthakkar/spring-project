7e910de2d3b2a2d1f92de31734eba515de743016
Falguni thakkar <falgunithakkar663@gmail.com>
2022-12-19 22:10:06 +0530

    3rd commit

---
+++ src/app/consumer/consumer-update/consumer-update.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Consumer } from 'src/app/consumer';
+import { ConsumerService } from 'src/app/consumer.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-consumer-update',
+  templateUrl: './consumer-update.component.html',
+  styleUrls: ['./consumer-update.component.css']
+})
+export class ConsumerUpdateComponent  implements OnInit{
+  [x: string]: any;
+
+  consumers!:Consumer[];
+  consumer!:Consumer;
+id!:number
+  update!:Consumer;
+    constructor(private consumerService:ConsumerService ){
+     // this.id=this.route.snapshot.paramMap
+    }
+   ngOnInit(){
+   }
+  
+   onConsumerUpdate(){
+    console.log(this.consumer)
+   this.consumerService.updatebyId(1).subscribe(consumer=>{
+    this.consumer=consumer;
+    
+   })
+   }
+}
